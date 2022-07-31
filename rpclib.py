7a5ab9e1001fd178eb5efe052f0ffb4cef68b12b
Crimson-Crow <github@crimsoncrow.dev>
2022-07-31 17:01:30 -0400

    pydantic rework

--- rpclib.py
+++ rpclib.py
@@ -1,167 +1,173 @@
-import json
-import logging
-from traceback import format_exc
+from pprint import pprint
+from typing import Literal, Union, Optional
 
-from . import rpcerror, rpcrequest, rpcresponse
+import orjson
+from pydantic import BaseModel, validator, Extra, StrictStr, StrictInt, StrictFloat, Field, ValidationError
+from json import JSONDecodeError
+
+JSONSerializable = Union[dict, list, int, float, str, bool, None]
+
+# def orjson_dumps(v, *, default=None):
+#     # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+#     return orjson.dumps(v, default=default).decode()
 
 
-def rpcmethod(func):
-    """
-    Decorator
-    Sign the decorated method as JSON-RPC-Method
-    """
+class Error(BaseModel, extra=Extra.forbid):
+    code: StrictInt
+    message: StrictStr
+    data: Optional[JSONSerializable]
 
-    # Sign the function as JSON-RPC-Method
-    func.rpcmethod = True
 
-    # Return the function itself
-    return func
+class Request(BaseModel, extra=Extra.forbid):
+    jsonrpc: Literal['2.0']
+    method: StrictStr
+    params: Optional[Union[list, dict]]
+    id: Optional[Union[StrictStr, StrictInt, StrictFloat]]
+
+
+class Response(BaseModel, extra=Extra.forbid):
+    jsonrpc: Literal['2.0']
+    result: Optional[JSONSerializable]
+    error: Optional[Error]
+    id: Union[StrictStr, StrictInt, StrictFloat, None] = Field(...)  # id can be null, but is required
+
+    @validator('error', always=True)
+    def mutually_exclusive(cls, v, values):  # TODO mutually exclusive in fields set
+        if v is not None and values['result'] is not None:
+            raise ValueError("'result' and 'error' are mutually exclusive.")
+        return v
 
 
 class JsonRpc:
-    """
-    JSON-RPC
-    """
-    methods = {}
+    def __init__(self, methods: Optional[dict] = None):
+        self.methods = methods if methods is not None else {}
 
-    def __init__(self, methods=None):
-        """
-        Initializes the JSON-RPC-Class
+    @staticmethod
+    def _split_params(params):
+        positional_params = []
+        named_params = {}
+        if isinstance(params, list):
+            positional_params = params
+        elif isinstance(params, dict):
+            named_params = params.copy()
+            positional_params = named_params.pop('__args', [])
 
-        :param methods: Json-RPC-Methods. `None` or dictionary with
-            method names as keys and functions as values. Syntax::
+        return positional_params, named_params
 
-                {
-                    "<method_name>": <method_function>,
-                    ...
-                }
-        """
-
-        if methods is not None:
-            self.methods = methods
-
-    def _process_request(self, request):
-        # Response data
-        result = None
-        error = None
-
+    def _process_request(self, request_obj: dict) -> Optional[Response]:
         try:
-            # Request data
+            request = Request.parse_obj(request_obj)
+        except ValidationError as ve:
+            return Response(jsonrpc='2.0', error=Error(code=-32600, message='Invalid Request', data=str(ve)), id=None)
+        else:
+            is_notification = 'id' not in request.__fields_set__
             try:
-                jsonrpc = request["jsonrpc"]
-                method = request["method"]
+                method = self.methods[request.method]
             except KeyError:
-                print("jsonrpc or method fields not found")  # TODO DEBUG
-                print(request)
-                raise rpcerror.InvalidRequest()
+                if is_notification:
+                    # TODO logger
+                    return
+                return Response(jsonrpc='2.0',
+                                error=Error(code=-32601,
+                                            message='Method not found',
+                                            data=f'Method "{request.method}" not found'),
+                                id=request.id)
 
-            # Test validity
-            if jsonrpc != "2.0" or not method:
-                print("jsonrpc != 2.0 or method is null")  # TODO DEBUG
-                raise rpcerror.InvalidRequest()
-
-            # Locate method
-            if method not in self.methods:
-                # Check if requested method is signed as *rpcmethod*
-                _method = getattr(self, method, None)
-                if callable(_method) and getattr(_method, "rpcmethod", False):
-                    self.methods[method] = _method
-                else:
-                    raise rpcerror.MethodNotFound(
-                        data=f"Method name: '{method}'")
-
-            # Split positional and named params
-            positional_params, named_params = request.get_splitted_params()
-            # Call the method with parameters
-            result = self.methods[method](*positional_params, **named_params)
-        except rpcerror.JsonRpcError as err:
-            error = err
-        except Exception as err:
-            traceback_info = format_exc()
-
-            if isinstance(err, TypeError) and "positional argument" in str(err):
-                error = rpcerror.InvalidParams(data=traceback_info)
+            try:
+                positional_params, named_params = self._split_params(request.params)
+                result = method(*positional_params, **named_params)
+            except Exception as e:
+                if is_notification:
+                    # TODO logger
+                    return
+                return Response(jsonrpc='2.0',
+                                error=Error(code=-32603,
+                                            message='Internal error'),
+                                id=request.id)  # TODO implement Invalid params
+                # traceback_info = format_exc()
+                #
+                # if isinstance(err, TypeError) and "positional argument" in str(err):
+                #     error = rpcerror.InvalidParams(data=traceback_info)
+                # else:
+                #     error = rpcerror.InternalError(message=str(err),
+                #                                    data=traceback_info)
             else:
-                error = rpcerror.InternalError(message=str(err),
-                                               data=traceback_info)
+                if is_notification:
+                    # TODO logger
+                    return
+                return Response(jsonrpc='2.0', result=result, id=request.id)
 
-        if error:
-            # Logging error
-            logging.error(f"{error} -- {error.data}")
-
+    def _load_request(self, raw_request: Union[str, bytes]) -> Union[dict, list[dict], None]:
         try:
-            id = request["id"]  # TODO CHANGE
-        except KeyError:
-            pass
+            raw_request_obj = orjson.loads(raw_request)
+        except JSONDecodeError as jde:
+            return Response(jsonrpc='2.0',
+                            error=Error(code=-32700, message='Parse error', data=str(jde)),
+                            id=None).dict(exclude_unset=True)
         else:
-            return rpcresponse.Response(
-                jsonrpc="2.0",
-                result=result,
-                # error=error, #TODO TEMPORARY
-                id=id)
+            if isinstance(raw_request_obj, list):  # batch request
+                if not raw_request_obj:
+                    return Response(jsonrpc='2.0',
+                                    error=Error(code=-32600, message='Invalid Request', data='empty batch request'),
+                                    id=None).dict(exclude_unset=True)
+                responses = []
+                for request_obj in raw_request_obj:
+                    if response := self._process_request(request_obj):
+                        responses.append(response.dict(exclude_unset=True))
+                if responses:
+                    return responses
+            else:
+                if response := self._process_request(raw_request_obj):
+                    return response.dict(exclude_unset=True)
 
-    def call(self, json_request):
-        """
-        Parses the *json_request*, calls the function(s)
-        and returns the *json_response*.
+    def call(self, request: Union[str, bytes]) -> bytes:
+        return orjson.dumps(self._load_request(request))  # TODO determine what happens if encoding error
 
-        :param json_request: JSON-RPC-string with one or more JSON-RPC-requests
 
-        :return: JSON-RPC-string with one or more responses or None if no answer is expected.
-        """
+# def create_notification_dict(method, *args, **kwargs):
+#     """
+#     Returns a notification JSON-RPC-dictionary for a method
+#
+#     :param method: Name of the method
+#     :param args: Positional parameters
+#     :param kwargs: Named parameters
+#     """
+#
+#     data = {"jsonrpc": "2.0", "method": str(method)}
+#
+#     if kwargs:
+#         params = kwargs
+#         if args:
+#             params["__args"] = args
+#     else:
+#         params = args
+#
+#     if params:
+#         data["params"] = params
+#     return data
 
-        try:
-            # Single Request object or list of Request objects (batch)
-            requests = rpcrequest.parse_request_json(json_request)
-        except rpcerror.JsonRpcError as err:
-            logging.error(f"{err} -- {err.data}")
-            ret = rpcresponse.Response(jsonrpc="2.0", error=err, id=None)
-        else:
-            ret = [self._process_request(request)
-                   for request in requests] if isinstance(
-                requests, list) else self._process_request(requests)
 
-        # TODO Check array of None for batch notifications
-        # Return as JSON-string (batch or normal)
-        if ret:
-            return json.dumps(ret)
-
-        # OLD CODE
-        # # List for the responses
-        # responses = []
-
-        # # Every JSON-RPC request in a batch of requests (or a wrapped single request)
-        # for request in requests if isinstance(requests, list) else [requests]:
-
-        # # Return as JSON-string (batch or normal)
-        # if responses:
-        #     return rpcjson.dumps(
-        #         responses if isinstance(requests, list) else responses[0])
-
-    def __call__(self, json_request):
-        """
-        Redirects the requests to *self.call*
-        """
-
-        return self.call(json_request)
-
-    def __getitem__(self, key):
-        """
-        Gets back the requested method
-        """
-
-        return self.methods[key]
-
-    def __setitem__(self, key, value):
-        """
-        Appends or replaces a method
-        """
-
-        self.methods[key] = value
-
-    def __delitem__(self, key):
-        """
-        Deletes a method
-        """
-
-        del self.methods[key]
+if __name__ == '__main__':
+    rpc_methods = {'subtract': lambda a, b: a - b}
+    rpc = JsonRpc(rpc_methods)
+    req = '[]'
+    print(rpc.call(req))
+    req = '{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1'
+    print(rpc.call(req))
+    req = '{"jsonrpc": "3.0", "method": "subtract", "params": [42, 23], "id": 1}'
+    print(rpc.call(req))
+    req = '{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}'
+    print(rpc.call(req))
+    req = '{"jsonrpc": "2.0", "method": 3, "params": [42, 23], "id": 1}'
+    print(rpc.call(req))
+    # req = '[1,2,3]'
+    # rpc.call(req)
+    req = '''[
+        {"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},
+        {"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},
+        {"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},
+        {"foo": "boo"},
+        {"jsonrpc": "2.0", "method": 1, "params": {"name": "myself"}, "id": "5"},
+        {"jsonrpc": "2.0", "method": "get_data", "id": "9"}
+    ]'''
+    print(rpc.call(req))
